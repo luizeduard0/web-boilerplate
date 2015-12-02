27f196b84513200fa9930d3f530b5f050c37cb12
Héliton Nordt <hnordt@hnordt.com>
2015-12-02 14:48:04 -0200

    redux-form

--- src/rootReducer.js
+++ src/rootReducer.js
@@ -1,7 +1,9 @@
 import { combineReducers } from 'redux';
+import { reducer as form } from 'redux-form';
 import projects from './modules/projects/reducer.js';
 
 const rootReducer = combineReducers({
+  form,
   projects
 });
 

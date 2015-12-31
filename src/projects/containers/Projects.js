d788e492376066766d7685fe92d7304882ec92ee
Héliton Nordt <hnordt@hnordt.com>
2015-12-31 19:16:25 -0200

    Closes #11

--- src/projects/containers/Projects.js
+++ src/projects/containers/Projects.js
@@ -1,15 +1,26 @@
 import React from 'react';
 import { connect, PromiseState } from 'react-refetch';
 import Async from '@hnordt/reax-async';
+import Row from '@hnordt/reax-row';
+import Col from '@hnordt/reax-col';
 import ProjectPanel from '../components/ProjectPanel.js';
+import ProjectForm from './ProjectForm.js';
 
 const AsyncProjectPanel = Async(ProjectPanel);
 
 const Projects = ({ projectsFetch }) => (
-  <AsyncProjectPanel
-    projects={projectsFetch.value}
-    isLoading={projectsFetch.pending}
-    error={projectsFetch.reason} />
+  <Row>
+    <Col md={8}>
+      <AsyncProjectPanel
+        projects={projectsFetch.value}
+        isLoading={projectsFetch.pending}
+        error={projectsFetch.reason}
+      />
+    </Col>
+    <Col md={4}>
+      <ProjectForm />
+    </Col>
+  </Row>
 );
 
 export default connect(() => ({

f148edeb1fa870c12f0b7a26c603101fa4990cb2
Héliton Nordt <hnordt@hnordt.com>
2016-01-03 20:21:05 -0200

    Reax API

--- src/projects/containers/Projects.js
+++ src/projects/containers/Projects.js
@@ -1,28 +1,73 @@
-import React from 'react';
-import { connect, PromiseState } from 'react-refetch';
+import React, { Component, PropTypes } from 'react';
+import { connect } from 'react-redux';
 import Async from '@hnordt/reax-async';
 import Row from '@hnordt/reax-row';
 import Col from '@hnordt/reax-col';
+import { callAPI } from '@hnordt/reax-api';
 import ProjectPanel from '../components/ProjectPanel.js';
 import ProjectForm from './ProjectForm.js';
 
 const AsyncProjectPanel = Async(ProjectPanel);
 
-const Projects = ({ projectsFetch }) => (
-  <Row>
-    <Col md={8}>
-      <AsyncProjectPanel
-        projects={projectsFetch.value}
-        isLoading={projectsFetch.pending}
-        error={projectsFetch.reason}
-      />
-    </Col>
-    <Col md={4}>
-      <ProjectForm />
-    </Col>
-  </Row>
-);
+class Projects extends Component {
+  componentDidMount() {
+    const { loadProjects } = this.props;
+    loadProjects();
+  }
+  render() {
+    const {
+      projects,
+      isLoading,
+      error,
+      loadProjects
+    } = this.props;
+    return (
+      <Row>
+        <Col md={8}>
+          <AsyncProjectPanel
+            projects={projects}
+            isLoading={isLoading}
+            error={error && (
+              <span>
+                Failed to load projects.
+                {' '}
+                <a href="#" onClick={event => {
+                  event.preventDefault();
+                  loadProjects();
+                }}>Try Again</a>
+              </span>
+            )}
+          />
+        </Col>
+        <Col md={4}>
+          <ProjectForm />
+        </Col>
+      </Row>
+    );
+  }
+}
 
-export default connect(() => ({
-  projectsFetch: '/api/projects.json'
-}))(Projects);
+Projects.propTypes = {
+  data: PropTypes.any,
+  isLoading: PropTypes.bool,
+  error: PropTypes.bool
+};
+
+const mapStateToProps = state => {
+  const {
+    data: projects,
+    isLoading = true,
+    error
+  } = state.api.projects || {};
+  return {
+    projects,
+    isLoading,
+    error
+  };
+};
+
+const mapDispatchToProps = dispatch => ({
+  loadProjects: () => dispatch(callAPI('projects', 'GET', '/api/projects.json'))
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(Projects);

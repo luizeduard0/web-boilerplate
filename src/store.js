9c7e38249c4ecf7d5067750604385f654b260404
Héliton Nordt <hnordt@hnordt.com>
2016-02-21 07:43:41 -0300

    devToolsExtension support

--- src/store.js
+++ src/store.js
@@ -1,16 +1,19 @@
-import { createStore, applyMiddleware } from 'redux'
+import { createStore, compose, applyMiddleware } from 'redux'
 import thunk from 'redux-thunk'
 import createLogger from 'redux-logger'
 import rootReducer from './rootReducer'
 
 const store = createStore(
   rootReducer,
-  applyMiddleware(
-    thunk,
-    createLogger({
-      duration: true,
-      predicate: () => process.env.NODE_ENV !== 'production'
-    })
+  compose(
+    applyMiddleware(
+      thunk,
+      createLogger({
+        duration: true,
+        predicate: () => process.env.NODE_ENV !== 'production'
+      })
+    ),
+    devToolsExtension ? devToolsExtension() : f => f
   )
 )
 
